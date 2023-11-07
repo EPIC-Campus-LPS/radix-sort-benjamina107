7f9285d3b0813788fed02227fc28414103318230
benjamina107 <benjamina107@lpsk12.org>
2023-11-07 11:25:13 -0700

    c

--- RadixSort.java
+++ RadixSort.java
@@ -1,29 +1,29 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Queue;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
 
 public class RadixSort {
 
     private ArrayList<String> list = new ArrayList<String>();
 
-    public RadixSort() {
+    public RadixSort(File f) {
 
-        File file = new File("file_name.txt");
+        File file = f;
 
         try {
             Scanner scanner = new Scanner(file);
 
             while (scanner.hasNextLine()) {
                 String i = scanner.nextLine();
-                list.add(i);
+                if (i != "")
+                    list.add(i);
             }
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        System.out.println(list);
     }
 
     public ArrayList<String> sort() {
@@ -38,14 +38,21 @@ public class RadixSort {
 
         //for each word length possible / "digit" in letter
         for (int i = longestLength; i >= 0; i--) {
-            ArrayList<String> temp = new ArrayList<String>();
+            ArrayList<String> temp = new ArrayList<String>(26);
+            for (int j = 0; j < 26; j++) {
+                temp.add(null);
+            }
+            ArrayList<String> q = new ArrayList<String>();
             //for each string in the list
             for (String s : list) {
-                //if it can be sorted then sort it, if not send it to tbe back
-
+                //if it can be sorted then sort it, if not send it to the back
                 if (s.length() >= i) {
                     //gets the character to compare
-                    String c = s.substring(i, i+1);
+                    String c = "";
+                    if(i == 0)
+                        c = String.valueOf(s.charAt(0));
+                    else
+                        c = String.valueOf(s.charAt(i-1));
                     //adds in position to arraylist based on char
                     switch(c) {
                         case "A":
@@ -64,32 +71,117 @@ public class RadixSort {
                         case "d":
                             temp.add(4, s);
                             break;
-                            //TODO finish this letter thing
-
-
-
-
-
-
-
-
-
-
-
-
+                        case "E":
+                        case "e":
+                            temp.add(5, s);
+                            break;
+                        case "F":
+                        case "f":
+                            temp.add(6, s);
+                            break;
+                        case "G":
+                        case "g":
+                            temp.add(7, s);
+                            break;
+                        case "H":
+                        case "h":
+                            temp.add(8, s);
+                            break;
+                        case "I":
+                        case "i":
+                            temp.add(9, s);
+                            break;
+                        case "J":
+                        case "j":
+                            temp.add(10, s);
+                            break;
+                        case "K":
+                        case "k":
+                            temp.add(11, s);
+                            break;
+                        case "L":
+                        case "l":
+                            temp.add(12, s);
+                            break;
+                        case "M":
+                        case "m":
+                            temp.add(13, s);
+                            break;
+                        case "N":
+                        case "n":
+                            temp.add(14, s);
+                            break;
+                        case "O":
+                        case "o":
+                            temp.add(15, s);
+                            break;
+                        case "P":
+                        case "p":
+                            temp.add(16, s);
+                            break;
+                        case "Q":
+                        case "q":
+                            temp.add(17, s);
+                            break;
+                        case "R":
+                        case "r":
+                            temp.add(18, s);
+                            break;
+                        case "S":
+                        case "s":
+                            temp.add(19, s);
+                            break;
+                        case "T":
+                        case "t":
+                            temp.add(20, s);
+                            break;
+                        case "U":
+                        case "u":
+                            temp.add(21, s);
+                            break;
+                        case "V":
+                        case "v":
+                            temp.add(22, s);
+                            break;
+                        case "W":
+                        case "w":
+                            temp.add(23, s);
+                            break;
+                        case "X":
+                        case "x":
+                            temp.add(24, s);
+                            break;
+                        case "Y":
+                        case "y":
+                            temp.add(25, s);
+                            break;
+                        case "Z":
+                        case "z":
+                            temp.add(26, s);
+                            break;
                     }
+                } else {
+                    //if not the right length to be sorted then add to back
+                    q.add(s);
                 }
-
-
-
             }
-
-
-
+            //adds the currently too-short strings to the back
+            for (int k = 0; k < q.size(); k++) {
+                String gggg = q.get(k);
+                temp.add(gggg);
+            }
+            temp.removeAll(Collections.singleton(null));
+            //replaces list with temp
+            list = temp;
         }
+        return list;
+    }
 
+    public static void main(String[] args) {
 
-
+        File file = new File("words.txt");
+        RadixSort radix = new RadixSort(file);
+        System.out.println(radix.sort());
 
     }
 }
